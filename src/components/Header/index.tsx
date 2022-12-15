db0f6ae72eeb5f98523c2392b5c528054b301a1f
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-12-15 14:30:09 -0300

    personalizando tema e inserindo dados mockados

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { ToggleTheme } from './ToggleTheme';
 
 export function Header() {
 	return (
-		<header className="flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm:m-5">
+		<header className="flex h-20 bg-alura-200 dark:bg-dark-200 justify-between items-center px-5 sm:rounded-xl sm:m-5">
 			<span className="text-gray-100">Olá, Usuário</span>
 			<h1 className="text-gray-100 text-xl">Alura Newsletter</h1>
 			<ToggleTheme />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,18 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+			colors: {
+				dark: {
+					100: 'rgb(18, 18, 18)',
+					200: 'rgb(51, 51, 51)'
+				},
+				alura: {
+					100: '#16fbf7',
+					200: '#051933'
+				}
+			},
+		},
   },
   plugins: [],
 }
