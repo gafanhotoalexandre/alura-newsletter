4e2ee4642f6aa7e58d5ffd440184f70eea284ab1
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-12-15 15:49:37 -0300

    listando tags e fix nas cor padrao alura

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -7,11 +7,21 @@ interface ArticleProps {
 export function Article({ article }: ArticleProps) {
 	return (
 		<div
-			className="p-5 bg-gray-200 flex flex-col items-center dark:bg-dark-200 sm:rounded-xl sm:shadow-lg"
+			className="p-5 bg-gray-200 flex flex-col gap-2 items-center dark:bg-dark-200 sm:rounded-xl sm:shadow-lg"
 		>
 			<h3
 				className="text-xl text-alura-200 dark:text-gray-200 font-bold"
 			>{article.title}</h3>
+
+			{/* tags */}
+			<div className="w-full sm:justify-end sm:gap-2 pr-5 hidden sm:flex">
+				{article.tags.map((tag) => (
+					<span
+						className="text-xs uppercase font-bold text-gray-200 bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full"
+						key={tag}
+					>{tag}</span>
+				))}
+			</div>
 
 			<div
 				className="flex flex-col gap-1"
