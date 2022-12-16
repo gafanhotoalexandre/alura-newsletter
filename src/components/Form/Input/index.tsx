baef57806ebd0cc79a30c8ae8b3717cf48c8f0e1
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-12-15 21:31:15 -0300

    refatorando estilos (utilizando @layers e @apply)

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -12,7 +12,7 @@ export function Input({
 	return (
 		<input
 			type={type}
-			className="py-1 pl-5 w-full max-w-lg dark:bg-dark-200 dark:text-gray-200 text-dark-200 rounded-full outline-none placeholder:text-gray-400 dark:placeholder:text-gray-600 placeholder:text-xs placeholder:uppercase placeholder:font-bold focus:ring-2 focus:ring-alura-100 focus:ring-offset-2 focus:ring-offset-gray-200 dark:focus:ring-offset-dark-100 focus:outline-none transition-all"
+			className="alura-input alura-focus-ring"
 			placeholder={placeholder}
 			onChange={(e) => setData(e.target.value)}
 			value={value}
